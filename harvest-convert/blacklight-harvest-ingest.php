ca3fb9294dbeea8dad261d93a28d2b69adab2d8e
Jeff <you@example.com>
2015-01-12 15:50:05 -0700

    Added post.jar and solr convert output dir

--- harvest-convert/blacklight-harvest-ingest.php
+++ harvest-convert/blacklight-harvest-ingest.php
@@ -10,21 +10,21 @@ require('oaidocs/oai-to-solr/OaiToSolrXmlParser.php');
 $harvester = new ADR_OAIHarvester();
 $parser = new OaiToSolrXmlParser();
 
-$curDate = date('Y-d-m');
+$curDate = date('Y-m-d');
 $dateFile = 'harvest.dat';
-$hdlDate = fopen($dateFile, 'rw');
+$hdlDate = fopen($dateFile, 'r+');
 $strDate = fread($hdlDate, filesize($dateFile));
 
-
-
-echo "*** Blacklight-DU Automatic Harvest and Index ***\n";
+echo "\n*** Blacklight-DU Automatic Harvest and Index ***\n";
 echo "Today's date: " . $curDate . "\n";
-echo "Most recent harvest prior to today: " . $strDate
+echo "Previous harvest date: " . $strDate . "\n";
 echo "Beginning harvest of OAI records from " . $strDate . " to present...\n";
-//$harvester->harvest_sets(null,$strDate);
+
+$harvester->harvest_sets(null,$strDate);
 
 // Remove existing date, and add the current date of this harvest.  The next auto-harvest will harvest from this date onward.
 //ftruncate($hdlDate, 0);
+//fwrite($hdlDate, $curDate);
 
 fclose($hdlDate);
 
