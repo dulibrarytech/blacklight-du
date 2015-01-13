8023dc6778cd62dd5f6a0277f61cb2876d30f31f
Jeff <you@example.com>
2015-01-13 11:43:42 -0700

    Pre-test ok

--- harvest-convert/blacklight-harvest-ingest.php
+++ harvest-convert/blacklight-harvest-ingest.php
@@ -34,16 +34,16 @@ echo "Previous harvest date: " . $strDate . "\n";
 // Harvest data
 // Harvester is currently set to output to the path oaidocs/oai-solr.  
 echo "Beginning harvest of OAI records from " . $strDate . " to present...\n";
-//$harvester->harvest_sets(null,$strDate);
+$harvester->harvest_sets(null,$strDate);
 echo "Harvest complete.\n";
 
 // Remove existing date, and add the current date of this harvest.  The next auto-harvest will harvest from this date onward.
-//ftruncate($hdlDate, 0);
-//fwrite($hdlDate, $curDate);
+ftruncate($hdlDate, 0);
+fwrite($hdlDate, $curDate);
 
 // Parse harvested xml to solr index xml
 echo "Parsing harvested data to solr xml...\n";
-//$parser->parseOAI();
+$parser->parseOAI();
 echo "Parse complete.\n";
 
 // Post index files to solr
@@ -58,7 +58,7 @@ if(!file_exists('oaidocs/harvester/docs')) {
 
 // Store harvested files in docs/ folder
 echo "Moving harvested files to docs/...\n";
-echo shell_exec('mv oaidocs/oai-to-solr/*.xml oaidocs/harvester/docs/.') . "\n"; 
+echo shell_exec('mv oaidocs/oai-to-solr/*.xml oaidocs/harvester/docs/') . "\n"; 
 
 // Remove solr index files, so they are not re-indexed next time
 echo "Removing solr index files...\n";
@@ -66,7 +66,7 @@ echo shell_exec('rm oaidocs/oai-to-solr/oai-dc-converted/*.xml') . "\n";
 
 // Write output to file
 $output = ob_get_flush();
-file_put_contents($logFile, $output)
+file_put_contents($logFile, $output);
 
 // Close files
 fclose($hdlDate);
